42514bc8e78ba62b0377b2aba6b11cfa103388a5
SkfyZ <SkfyZ@outlook.com>
2022-11-28 13:26:50 +0800

    1.6.1.1

---
+++ user/env.js
@@ -0,0 +1,26 @@
+window.settings = {
+  // 站点标题
+  title: 'DegYax With Love',
+  // 站点描述
+  description: '💝お帰りなさい💝',
+  // API
+  host: 'https://api.degyax.com',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: 'https://dmoe.sskfy.com',
+  // crisp
+  crisp_id: '',
+  i18n: [
+    'zh-CN',
+    'en-US',
+    'ja-JP',
+    'vi-VN',
+    'ko-KR',
+    'zh-TW'
+  ]
+}
